e29c7faf328d5eaf91de63a06571195903ad052f
Faith Okoth <faithokoth@Faiths-MacBook-Pro.local>
2024-02-02 11:18:22 +0300

    print 2 digit numbers

---
+++ 0x00-java-if_else_loops_functions/print_comb2.java
@@ -0,0 +1,13 @@
+/*
+ * print 0 to 99
+ */
+public class print_comb2 {
+    public static void main(String [] args) {
+        for (int i = 0;i < 99; ++i){
+            System.out.printf("%02d, ", i);
+            
+        }
+        System.out.printf("%d\n", 99);
+    }
+    
+}
