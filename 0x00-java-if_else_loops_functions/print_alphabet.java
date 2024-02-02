346037d28c1af8497842bb874d5181219939c0c0
Faith Okoth <faithokoth@Faiths-MacBook-Pro.local>
2024-02-02 09:16:48 +0300

    print alphabet in lowercase

---
+++ 0x00-java-if_else_loops_functions/print_alphabet.java
@@ -0,0 +1,16 @@
+//package 0x00-java-if_else_loops_functions;
+/*
+ * print the alphabet in lower case
+ */
+
+public class print_alphabet {
+    public static void main(String[] args) {
+        for(char i = 'a'; i <= 'z'; ++i) {
+            System.out.print(i);
+
+        }
+        System.out.print("\n");
+    }
+
+    
+}
