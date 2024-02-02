3aa3d95a3ad8290df1f107f358b05eb64de33883
Faith Okoth <faithokoth@Faiths-MacBook-Pro.local>
2024-02-02 09:05:52 +0300

    updated condition

--- 0x00-java-if_else_loops_functions/last_digit.java
+++ 0x00-java-if_else_loops_functions/last_digit.java
@@ -8,12 +8,12 @@ public class last_digit {
         int lastdigit = number % 10;
         String statement = "";
         if (lastdigit > 5) {
-            statement = "and is greater than 5";
-        }if (lastdigit < 5) {
-            statement = " and is less than 5";
+            statement = " and is greater than 5";
+        }if (lastdigit < 6 && lastdigit != 0) {
+            statement = " and is less than 6 and not 0";
             
         } else {
-            statement = "and is 0";
+            statement = " and is 0";
             
         }
         System.out.println("Last digit of " + number + " is " + lastdigit + statement);
