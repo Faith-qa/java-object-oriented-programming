aabe5991cc201ed370c9e8ae118f0c8a7be15d7a
Faith Okoth <faithokoth@Faiths-MacBook-Pro.local>
2024-02-02 10:57:23 +0300

    print hexadecimal

---
+++ 0x00-java-if_else_loops_functions/print_hexa.java
@@ -0,0 +1,12 @@
+/**
+ * prints all numbers from 0 to 98
+ * in decimal and hexa
+ */
+public class print_hexa {
+    public static void main(String [] args) {
+        for (int i = 0; i < 99; ++i){
+            System.out.printf("%d = 0x%X%n", i,i);
+        }
+    }
+    
+}
