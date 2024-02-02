1e0a07f72d05c8e293a51f0da4fe1b75a464c311
Faith Okoth <faithokoth@Faiths-MacBook-Pro.local>
2024-02-02 09:02:11 +0300

    last digit of number

---
+++ 0x00-java-if_else_loops_functions/last_digit.java
@@ -0,0 +1,24 @@
+import java.util.Random;
+
+public class last_digit {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int number = random.nextInt(20001) - 10000;
+        // YOUR CODE HERE
+        int lastdigit = number % 10;
+        String statement = "";
+        if (lastdigit > 5) {
+            statement = "and is greater than 5";
+        }if (lastdigit < 5) {
+            statement = " and is less than 5";
+            
+        } else {
+            statement = "and is 0";
+            
+        }
+        System.out.println("Last digit of " + number + " is " + lastdigit + statement);
+
+    }
+
+    
+}
