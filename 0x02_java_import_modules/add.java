badc841b8a03daef6387346682ebb480ac732b6f
Faith Okoth <faithokoth@Faiths-MacBook-Pro.local>
2024-02-20 21:18:33 +0300

    add

---
+++ 0x02_java_import_modules/add.java
@@ -0,0 +1,20 @@
+//package 0x02_java_import_modules;
+/*
+ * add two elements
+ */
+public class add {
+    public static int add(int a, int b){
+        return(a + b);
+
+        
+
+    }
+
+    public static void main(String[] args){
+        int result = add(3, 5);
+        System.out.println("Result: " + result);
+    }
+
+
+    
+}
