bf06231b14e806d0dde392e751ab8cd25736f4dd
Faith Okoth <faithokoth@Faiths-MacBook-Pro.local>
2024-02-21 10:52:49 +0300

    created an empty class called square

---
+++ 0x06_java_classes/square.java
@@ -0,0 +1,14 @@
+//create an empty class called square
+
+class Square{
+
+    public static void main(String[] args){
+        
+        Square mysquare = new Square();
+
+        System.out.println(mysquare);
+
+    }
+
+}
+
